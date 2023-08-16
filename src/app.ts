3b4934673fdf24c3c98d7a1196e1429ba330c5d9
Mikael <mikaelmuniz2001@gmail.com>
2023-08-16 16:37:55 -0300

    logger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import Koa from 'koa';
 import user from './routes/user.routes';
 
+const logger = require('koa-logger')
 const app = new Koa();
 
+app.use(logger());
 app.use(user.routes());
 
 export default app;
